d91c0f90442ca0e688cfc8e47c95bc9e365ca2b0
Adam Marshall <adamm3992@gmail.com>
2018-02-25 14:36:06 -0500

    fix incorrect use of TEST_END

--- t/20-extras.cpp
+++ t/20-extras.cpp
@@ -10,13 +10,13 @@
 using namespace TAP;
 
 int main() {
-  TEST_START(1);
+  TEST_START(2);
 
-  //TRY(throw 1, "throws an exception");
+  TRY_NOT_OK(throw 1, "throws an exception");
 
   TRY_DECL(int age = 5, "declare age to be 5");
 
-  TEST_END(1);
+  TEST_END;
 
   return 0;
 }
