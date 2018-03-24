d1123e50120e91e49e1974397c4efb3db321e012
Adam Marshall <adamm3992@gmail.com>
2018-03-24 14:00:58 -0400

    add doc and some tests to 20-extra

--- t/20-extras.cpp
+++ t/20-extras.cpp
@@ -1,7 +1,9 @@
 /*
- * 20-extras.cpp
+ * \file 20-extras.cpp
  *
- * demonstrate test extras
+ * \author Adam Marshall (ih8celery)
+ *
+ * \brief demonstrate test extras
  */
 
 #define WANT_TEST_EXTRAS
@@ -12,26 +14,36 @@ using namespace TAP;
 int main() {
   plan(5);
 
+  note("tests are running in the outermost test group");
+
   TRY_NOT_OK(throw 1, "throws an exception");
 
   skip(2);
   ok(false, "false is ok");
   note("the condition of a skipped test is not evaluated");
 
-  SUBTEST(1, "anonymous") DO
-    pass("running a test in an anonymous subtest");
+  SUBTEST(1, "first subtest") DO
+    pass("running a test in a subtest");
   END
 
-  SUBTEST(3, "second subtest") DO
+  SUBTEST(4, "second subtest") DO
     ok(1, "test one");
+
     todo();
     isnt(1, 0, "1 is not 0");
+    
     is_convertible<int, double>("int convertible to double");
+
+    DO
+      note("in nested anonymous subtest");
+      not_ok(false, "false is actually not ok");
+      ok(true, "true is ok");
+    END
   END
 
   note("almost done testing");
 
-  ok(true, "last second test");
+  ok(true, "last-second test");
 
   done_testing();
 
