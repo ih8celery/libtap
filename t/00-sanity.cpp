e89c24bf7c0cb7301eced2141d14f8e70f05851e
Adam Marshall <adamm3992@gmail.com>
2018-03-24 13:15:29 -0400

    add docs and more tests to 00-sanity

--- t/00-sanity.cpp
+++ t/00-sanity.cpp
@@ -1,7 +1,9 @@
-/*
- * 00-sanity.cpp
+/**
+ * \file 00-sanity.cpp
+ * 
+ * \author Adam Marshall (ih8celery)
  *
- * perform the most basic tests of libtap, not going into great depth
+ * \brief perform the most basic tests of libtap, not going into great depth
  */
 
 #include "tap++.h"
@@ -9,7 +11,7 @@
 using namespace TAP;
 
 int main() {
-  plan(5);
+  plan(9);
 
   ok(1, "the value 1 is true");
 
@@ -17,10 +19,20 @@ int main() {
 
   ok(true, "true is true");
 
+  ok(encountered() == 3, "found 3 tests here");
+
   ok(1 == 1, "1 == 1");
 
-  note("is is defined as a template");
-  is(5, 5, "5 is 5");
+  ok(planned() == 9, "planned 9 tests");
+
+  note("'is' is defined as a template");
+  is(6, 6, "6 is 6");
+
+  isnt(6, 7, "6 is not 7");
+
+  ok(details::test_status, "previous test succeeded");
+
+  note("\'", details::separator, "\' is the separator string");
 
   done_testing();
 
